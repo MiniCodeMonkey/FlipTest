381c666768c526009c90b1eb5935e18e27981d9d
Mathias Hansen <me@codemonkey.io>
2013-06-08 12:59:50 -0400

    Ignoring build folder

--- SDK/build/iOS-AB-Testing-Framework.build/Debug-iphonesimulator/iOS-AB-Testing-Framework.build/Objects-normal/i386/iOS_AB_Testing_Framework.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/mini/Documents/Projekter/iOS-AB-Testing-Framework/SDK/iOS-AB-Testing-Framework/iOS_AB_Testing_Framework.m \
-  /Users/mini/Documents/Projekter/iOS-AB-Testing-Framework/SDK/iOS-AB-Testing-Framework/iOS_AB_Testing_Framework.h
