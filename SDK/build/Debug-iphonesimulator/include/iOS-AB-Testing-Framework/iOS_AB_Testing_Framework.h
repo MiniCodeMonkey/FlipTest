177b482e19481e05490e2ca86bd695f3b9642a11
Mathias Hansen <me@codemonkey.io>
2013-06-08 12:58:45 -0400

    Added Base SDK

---
+++ SDK/build/Debug-iphonesimulator/include/iOS-AB-Testing-Framework/iOS_AB_Testing_Framework.h
@@ -0,0 +1,13 @@
+//
+//  iOS_AB_Testing_Framework.h
+//  iOS-AB-Testing-Framework
+//
+//  Created by Mathias Hansen on 6/8/13.
+//  Copyright (c) 2013 Mathias Hansen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOS_AB_Testing_Framework : NSObject
+
+@end
---
+++ SDK/build/iOS-AB-Testing-Framework.build/Debug-iphonesimulator/iOS-AB-Testing-Framework.build/Objects-normal/i386/iOS_AB_Testing_Framework.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/mini/Documents/Projekter/iOS-AB-Testing-Framework/SDK/iOS-AB-Testing-Framework/iOS_AB_Testing_Framework.m \
+  /Users/mini/Documents/Projekter/iOS-AB-Testing-Framework/SDK/iOS-AB-Testing-Framework/iOS_AB_Testing_Framework.h
